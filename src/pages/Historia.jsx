351b60951dd4b541008bcb30fbb17cd2ec8e60ca
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-03-23 18:09:14 -0300

    carousel, falta aplicar el script

--- src/pages/Historia.jsx
+++ src/pages/Historia.jsx
@@ -1,13 +1,206 @@
+import "../css/historia.css";
+import { Slider } from "../helpers/historia";
+
 export const Historia = () => {
   return (
     <>
+        <section id="tranding">
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-            <h2></h2>
+        <h3 className="text-center section-subheading">- popular Delivery -</h3>
+        <h1 className="text-center section-heading">Tranding food</h1>
+      </div>
+      <div className="container"/>
+        <div className="swiper tranding-slider">
+          <div className="swiper-wrapper">
+           
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$20</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Special Pizza
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+           
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$20</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Meat Ball
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+     
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$40</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Burger
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$15</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Frish Curry
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+           
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$15</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Pane Cake
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+     
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$20</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Vanilla cake
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+           
+            <div className="swiper-slide tranding-slide">
+              <div className="tranding-slide-img">
+                <img src="https://picsum.photos/399/532" alt="Tranding"/>
+              </div>
+              <div className="tranding-slide-content">
+                <h1 className="food-price">$8</h1>
+                <div className="tranding-slide-content-bottom">
+                  <h2 className="food-name">
+                    Straw Cake
+                  </h2>
+                  <h3 className="food-rating">
+                    <span>4.5</span>
+                    <div className="rating">
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                      <ion-icon name="star"></ion-icon>
+                    </div>
+                  </h3>
+                </div>
+              </div>
+            </div>
+         
           </div>
+
+          <div className="tranding-slider-control">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+
         </div>
       </div>
+      
+    </section>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <script src="../helpers/historia.js"></script>
     </>
   )
 }
