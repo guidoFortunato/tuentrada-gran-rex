6353ff5505d4f439d780a5c78c0a06bb8caa2ba3
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-03-23 11:40:00 -0300

    cambios home, imagenes grandes 1000x487px y css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import "../css/header.css";
-
+import "../css/footer.css";
 export const Home = () => {
   return (
     <>
@@ -19,14 +19,15 @@ export const Home = () => {
         <div className="container">
           <div className="row">
             <div className="col-12 text-center mt-3 ">
-              <h3>Próximos eventos</h3>
+              <h2 style={{fontSize:"35px"}}>Próximos eventos</h2>
             </div>
           </div>
+          <div >
           <div className="row">
-            <article className="col-12 col-lg-8">
+            <article className="col-12 col-lg-8 desktop">
               <div className="card">
                 <img
-                  src="https://picsum.photos/1000/500"
+                  src="https://picsum.photos/1000/487"
                   className="card-img-top card-img-bottom"
                   alt="img logo"
                   // style={{ width: "80%" }}
@@ -34,14 +35,16 @@ export const Home = () => {
               </div>
               <div className="card">
                 <img
-                  src="https://picsum.photos/1000/500"
+                  src="https://picsum.photos/1000/487"
                   className="card-img-top card-img-bottom"
                   alt="img logo"
                   // style={{ width: "80%" }}
                 />
               </div>
+            
             </article>
-            <article className="col-12 col-lg-4 card-media">
+
+            <article className="col-12 col-lg-4 card-media mobile" >
               <div className="card mb-20">
                 <img
                   src="https://picsum.photos/500/500"
@@ -59,8 +62,28 @@ export const Home = () => {
                 />
               </div>
             </article>
+           
+            <article className="col-12 col-lg-4 card-media" >
+              <div className="card mb-20">
+                <img
+                  src="https://picsum.photos/500/500"
+                  className="card-img-top card-img-bottom size-img"
+                  alt="img logo"
+                  // style={{ width: "80%" }}
+                />
+              </div>
+              <div className="card">
+                <img
+                  src="https://picsum.photos/500/500"
+                  className="card-img-top card-img-bottom size-img"
+                  alt="img logo"
+                  // style={{ width: "80%" }}
+                />
+              </div>
+            </article>
+       
           </div>
-
+          </div>
           <div className="row sin-padding-right-left">
             <article className="col-12 col-md-6 col-lg-4">
               <div className="card">
@@ -124,7 +147,69 @@ export const Home = () => {
             </article>
           </div>
         </div>
-      </main>
+      </main> 
+      <div className="container">
+        <footer className="py-5" style={{width:"100%",
+    padding:"inherit"}}>
+          <div className="row">
+            <div className="col-6 col-md-2 mb-3">
+              <h5>Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+
+            <div className="col-6 col-md-2 mb-3">
+              <h5>Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+
+            <div className="col-6 col-md-2 mb-3">
+              <h5>Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+              </ul>
+            </div>
+
+            <div className="col-md-5 offset-md-1 mb-3">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+                  <label for="newsletter1" className="visually-hidden">Email address</label>
+                  <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+                  <button className="btn btn-primary" type="button">Subscribe</button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <p>© 2022 Company, Inc. All rights reserved.</p>
+            <ul className="list-unstyled d-flex">
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    
     </>
   );
-};
+}
+
