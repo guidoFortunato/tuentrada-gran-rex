1a89cbbca2786685cab1b6d56a15bacffc628607
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-03-23 11:50:15 -0300

    agrego svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import "../css/header.css";
 import "../css/footer.css";
+import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <>
@@ -19,70 +20,68 @@ export const Home = () => {
         <div className="container">
           <div className="row">
             <div className="col-12 text-center mt-3 ">
-              <h2 style={{fontSize:"35px"}}>Próximos eventos</h2>
+              <h2 style={{ fontSize: "35px" }}>Próximos eventos</h2>
             </div>
           </div>
-          <div >
-          <div className="row">
-            <article className="col-12 col-lg-8 desktop">
-              <div className="card">
-                <img
-                  src="https://picsum.photos/1000/487"
-                  className="card-img-top card-img-bottom"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-              <div className="card">
-                <img
-                  src="https://picsum.photos/1000/487"
-                  className="card-img-top card-img-bottom"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-            
-            </article>
+          <div>
+            <div className="row">
+              <article className="col-12 col-lg-8 desktop">
+                <div className="card">
+                  <img
+                    src="https://picsum.photos/1000/487"
+                    className="card-img-top card-img-bottom"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+                <div className="card">
+                  <img
+                    src="https://picsum.photos/1000/487"
+                    className="card-img-top card-img-bottom"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+              </article>
 
-            <article className="col-12 col-lg-4 card-media mobile" >
-              <div className="card mb-20">
-                <img
-                  src="https://picsum.photos/500/500"
-                  className="card-img-top card-img-bottom size-img"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-              <div className="card">
-                <img
-                  src="https://picsum.photos/500/500"
-                  className="card-img-top card-img-bottom size-img"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-            </article>
-           
-            <article className="col-12 col-lg-4 card-media" >
-              <div className="card mb-20">
-                <img
-                  src="https://picsum.photos/500/500"
-                  className="card-img-top card-img-bottom size-img"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-              <div className="card">
-                <img
-                  src="https://picsum.photos/500/500"
-                  className="card-img-top card-img-bottom size-img"
-                  alt="img logo"
-                  // style={{ width: "80%" }}
-                />
-              </div>
-            </article>
-       
-          </div>
+              <article className="col-12 col-lg-4 card-media mobile">
+                <div className="card mb-20">
+                  <img
+                    src="https://picsum.photos/500/500"
+                    className="card-img-top card-img-bottom size-img"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+                <div className="card">
+                  <img
+                    src="https://picsum.photos/500/500"
+                    className="card-img-top card-img-bottom size-img"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+              </article>
+
+              <article className="col-12 col-lg-4 card-media">
+                <div className="card mb-20">
+                  <img
+                    src="https://picsum.photos/500/500"
+                    className="card-img-top card-img-bottom size-img"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+                <div className="card">
+                  <img
+                    src="https://picsum.photos/500/500"
+                    className="card-img-top card-img-bottom size-img"
+                    alt="img logo"
+                    // style={{ width: "80%" }}
+                  />
+                </div>
+              </article>
+            </div>
           </div>
           <div className="row sin-padding-right-left">
             <article className="col-12 col-md-6 col-lg-4">
@@ -147,41 +146,100 @@ export const Home = () => {
             </article>
           </div>
         </div>
-      </main> 
+      </main>
       <div className="container">
-        <footer className="py-5" style={{width:"100%",
-    padding:"inherit"}}>
+        <footer className="py-5" style={{ width: "100%", padding: "inherit" }}>
           <div className="row">
             <div className="col-6 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-muted">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-muted">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-muted">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-muted">
+                    FAQs
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link p-0 text-muted">
+                    About
+                  </a>
+                </li>
               </ul>
             </div>
 
             <div className="col-6 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Features
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Pricing
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    FAQs
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    About
+                  </Link>
+                </li>
               </ul>
             </div>
 
             <div className="col-6 col-md-2 mb-3">
               <h5>Section</h5>
               <ul className="nav flex-column">
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Features
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    Pricing
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    FAQs
+                  </Link>
+                </li>
+                <li className="nav-item mb-2">
+                  <Link to="#" className="nav-link p-0 text-muted">
+                    About
+                  </Link>
+                </li>
               </ul>
             </div>
 
@@ -190,26 +248,72 @@ export const Home = () => {
                 <h5>Subscribe to our newsletter</h5>
                 <p>Monthly digest of what's new and exciting from us.</p>
                 <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                  <label for="newsletter1" className="visually-hidden">Email address</label>
-                  <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
-                  <button className="btn btn-primary" type="button">Subscribe</button>
+                  <label htmlFor="newsletter1" className="visually-hidden">
+                    Email address
+                  </label>
+                  <input
+                    id="newsletter1"
+                    type="text"
+                    className="form-control"
+                    placeholder="Email address"
+                  />
+                  <button className="btn btn-primary" type="button">
+                    Subscribe
+                  </button>
                 </div>
               </form>
             </div>
           </div>
 
           <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-            <p>© 2022 Company, Inc. All rights reserved.</p>
+            <p>© 2023 Company, Inc. All rights reserved.</p>
             <ul className="list-unstyled d-flex">
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+              <li className="ms-3">
+                <a className="link-dark" rel="noreferrer" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 50 50"
+                  >
+                    <path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-dark" rel="noreferrer" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 50 50"
+                  >
+                    <path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-dark" rel="noreferrer" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 50 50"
+                  >
+                    <path d="M25,3C12.85,3,3,12.85,3,25c0,11.03,8.125,20.137,18.712,21.728V30.831h-5.443v-5.783h5.443v-3.848 c0-6.371,3.104-9.168,8.399-9.168c2.536,0,3.877,0.188,4.512,0.274v5.048h-3.612c-2.248,0-3.033,2.131-3.033,4.533v3.161h6.588 l-0.894,5.783h-5.694v15.944C38.716,45.318,47,36.137,47,25C47,12.85,37.15,3,25,3z"></path>
+                  </svg>
+                </a>
+              </li>
             </ul>
           </div>
         </footer>
       </div>
-    
     </>
   );
-}
-
+};
