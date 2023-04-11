16256a540eebcf5bda8444c0c1f7c05a0464c021
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-04-11 12:36:42 -0300

    cambios

--- src/components/DetalleEvento.jsx
+++ src/components/DetalleEvento.jsx
@@ -14,8 +14,8 @@ export const DetalleEvento = ({ title, description, href, img }) => {
 
   return (
     <>
-      <div className="container my-5 px-5 ">
-        <h1 style={{ fontSize: "25px" }}>{title.toUpperCase()}</h1>
+      <div className="container my-5 px-5 container animate__animated animate__fadeIn animate__fast ">
+        <h1 className="animate__animated animate__fadeIn" style={{ fontSize: "25px" }}>{title.toUpperCase()}</h1>
         <hr />
         <div className="row">
           <div className="col-12 col-lg-6 mb-5 mb-lg-0 text-center">
@@ -25,7 +25,7 @@ export const DetalleEvento = ({ title, description, href, img }) => {
               className="img-fluid"
             />
           </div>
-          <div className="col-12 col-lg-6 detalle-del-evento">
+          <div className="col-12 col-lg-6 detalle-del-evento animate__animated animate__fadeIn">
             <h2 className="fw-bold pb-2">Información general</h2>
             <p
               dangerouslySetInnerHTML={{
