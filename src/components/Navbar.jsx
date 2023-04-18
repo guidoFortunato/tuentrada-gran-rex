c55849217dbaf73aa2ed0cb820ae74bd7248de9f
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-04-18 17:13:33 -0300

    navbar provisorio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,15 +49,55 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-dark navbar-expand-lg color-navbar sticky-top animate__animated animate__fadeIn animate__faster">
-      <div className="container">
-        <div className="row w-100 justify-content-center">
-          <div className="col-12 container-redes-nav mb-5 mb-lg-3">
-            <div className="me-4">
-              <span className="text-light h2">Seguinos en nuestras redes</span>
+      <div className="container-fluid">
+        <Link className="navbar-brand ms-lg-4" to="/">
+          <img src={imgRex} alt="logo gran rex" style={{ width: "75px" }} />
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={!isNavCollapsed ? true : false}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span
+            style={{ fontSize: "16px" }}
+            className="navbar-toggler-icon"
+          ></span>
+        </button>
+        <div
+          className={`${
+            isNavCollapsed ? "collapse" : ""
+          } navbar-collapse justify-content-center`}
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ms-auto" style={{ fontSize: "18px" }}>
+            {itemsNavbar.map(
+              (item) =>
+                item.mostrar && (
+                  <li className="nav-item item-nav" key={item.id}>
+                    <Link
+                      className="nav-link border-navbar-color border-navbar"
+                      to={item.link}
+                      onClick={handleNavCollapse}
+                    >
+                      <span className="size-item-nav">{item.name}</span>
+                    </Link>
+                  </li>
+                )
+            )}
+          </ul>
+          <div className="d-flex align-items-center ms-lg-auto me-lg-4">
+            <div>
+              <span className="span-redes me-2">Seguinos en nuestras redes:</span>
             </div>
             <div>
-              <ul className="list-unstyled ms-auto d-flex mb-0">
-                <li className="me-3">
+              <ul className="list-unstyled d-flex mb-0">
+                <li className="me-4">
                   <a
                     className="link-dark"
                     rel="noreferrer"
@@ -80,7 +120,7 @@ export const Navbar = () => {
                     </svg>
                   </a>
                 </li>
-                <li className="me-3">
+                <li className="me-4">
                   <a
                     className="link-dark"
                     rel="noreferrer"
@@ -126,53 +166,6 @@ export const Navbar = () => {
                     </svg>
                   </a>
                 </li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="col-12 text-center mb-5">
-            <Link className="navbar-brand" to="/">
-              <img src={imgRex} alt="logo gran rex" style={{ width: "75px" }} />
-            </Link>
-          </div>
-
-          <div className="col-12">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded={!isNavCollapsed ? true : false}
-              aria-label="Toggle navigation"
-              onClick={handleNavCollapse}
-            >
-              <span
-                style={{ fontSize: "16px" }}
-                className="navbar-toggler-icon"
-              ></span>
-            </button>
-            <div
-              className={`${
-                isNavCollapsed ? "collapse" : ""
-              } navbar-collapse justify-content-center`}
-              id="navbarNav"
-            >
-              <ul className="navbar-nav" style={{ fontSize: "18px" }}>
-                {itemsNavbar.map(
-                  (item) =>
-                    item.mostrar && (
-                      <li className="nav-item item-nav" key={item.id}>
-                        <Link
-                          className="nav-link border-navbar-color border-navbar"
-                          to={item.link}
-                          onClick={handleNavCollapse}
-                        >
-                          <span className="size-item-nav">{item.name}</span>
-                        </Link>
-                      </li>
-                    )
-                )}
               </ul>
             </div>
           </div>
