8fb51e6e3b05db8f8a46d30b9e4e72e63b022694
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-04-25 12:36:17 -0300

    agrego componente imagen historia

--- src/pages/Historia.jsx
+++ src/pages/Historia.jsx
@@ -1,7 +1,7 @@
 // import { GaleriaHistoria } from "../components";
 import { useContext } from "react";
 import { EventosContext } from "../context/EventosProvider";
-import { Spinner } from "../components";
+import { ImagenHistoria, Spinner } from "../components";
 import DOMPurify from "dompurify";
 
 import "../css/historia.css";
@@ -11,10 +11,10 @@ export const Historia = () => {
   console.log({ eventosTotales });
 
   if (!dataNavbar) {
-    return <Spinner />
+    return <Spinner />;
   }
   if (!eventosTotales) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
@@ -26,21 +26,18 @@ export const Historia = () => {
               style={{ fontSize: "30px" }}
               className="my-3 animate__fadeIn animate__delay-1s tittle-h2"
             >
-              { dataNavbar !== null && dataNavbar[0]?.items[2].titulo1}
+              {dataNavbar !== null && dataNavbar[0].items[2].titulo1}
             </h2>
           </div>
         </div>
 
         <div className="col-12">
-          
           <p
-              className="parrafo-historia"
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(eventosTotales.historia.descripcion),
-              }}
-            ></p>
-          
-          
+            className="parrafo-historia"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(eventosTotales.historia.descripcion),
+            }}
+          ></p>
         </div>
       </div>
 
@@ -54,7 +51,7 @@ export const Historia = () => {
             style={{ fontSize: "30px" }}
             className="my-3 animate__fadeIn animate__delay-1s tittle-h2"
           >
-            {dataNavbar !== null && dataNavbar[0]?.items[2].titulo2}
+            {dataNavbar !== null && dataNavbar[0].items[2].titulo2}
           </h2>
         </div>
       </div>
@@ -63,146 +60,9 @@ export const Historia = () => {
         style={{ textAlign: "center", padding: "20px" }}
       >
         <div className="row">
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/1.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/2.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/3.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/4.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/5.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/6.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/7.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/8.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/9.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/10.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/11.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/12.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/13.webp"
-              alt="img-rex"
-            />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-5 p-2 galeria-img">
-            <img
-              style={{
-                boxShadow: "5px 5px 6px 5px rgba(128, 128, 128, 0.443)",
-                width: "250px",
-              }}
-              src="storage/imagenes/galeria/14.webp"
-              alt="img-rex"
-            />
-          </div>
+          {eventosTotales?.historia.imagenes.map((item) => (
+            <ImagenHistoria src={item.src} />
+          ))}
         </div>
       </div>
     </>
