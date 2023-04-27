aebd5921dad0fe58c0d8d45d1438cec6696cb922
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-04-27 17:19:01 -0300

    cambio vite config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "normalize.css"
+import 'normalize.css'
 import "/storage/css/index.css";
 
 import { BrowserRouter, HashRouter } from "react-router-dom";
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,63 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      
       external: [
-        "/storage/css/calendario.css",
-        "/storage/css/cardevento.css",
-        "/storage/css/comollegar.css",
-        "/storage/css/detalleevento.css",
-        "/storage/css/footer.css",
-        "/storage/css/header.css",
-        "/storage/css/historia.css",
-        "/storage/css/index.css",
-        "/storage/css/navbar.css",
-        "/storage/css/preguntasfrecuentes.css",
-        "/storage/css/sliderdestacado.css",
-        "/storage/css/tablaprecios.css"
+        {
+          id: "/storage/css/calendario.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/cardevento.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/comollegar.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/detalleevento.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/footer.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/header.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/historia.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/index.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/navbar.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/preguntasfrecuentes.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/sliderdestacado.css",
+          contentType: "text/css"
+        },
+        {
+          id: "/storage/css/tablaprecios.css",
+          contentType: "text/css"
+        }
       ],
     },
-    cssCodeSplit: false
   },
 });
 
