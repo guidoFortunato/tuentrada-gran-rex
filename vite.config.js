0a9700b3ef2dfc53bd70f5124060fa591d6c7344
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-04-27 15:57:32 -0300

    cambios en vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,19 +7,20 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        '/storage/css/calendario.css',
-        '/storage/css/cardevento.css',
-        '/storage/css/comollegar.css',
-        '/storage/css/detalleevento.css',
-        '/storage/css/footer.css',
-        '/storage/css/header.css',
-        '/storage/css/historia.css',
-        '/storage/css/index.css',
-        '/storage/css/navbar.css',
-        '/storage/css/preguntasfrecuentes.css',
-        '/storage/css/sliderdestacado.css',
-        '/storage/css/tablaprecios.css'
-      ]
-    }
-  }
-})
+        "/storage/css/calendario.css",
+        "/storage/css/cardevento.css",
+        "/storage/css/comollegar.css",
+        "/storage/css/detalleevento.css",
+        "/storage/css/footer.css",
+        "/storage/css/header.css",
+        "/storage/css/historia.css",
+        "/storage/css/index.css",
+        "/storage/css/navbar.css",
+        "/storage/css/preguntasfrecuentes.css",
+        "/storage/css/sliderdestacado.css",
+        "/storage/css/tablaprecios.css"
+      ],
+    },
+    cssCodeSplit: false
+  },
+});
