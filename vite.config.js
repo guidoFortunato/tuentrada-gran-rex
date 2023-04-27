126c41597a167e1deb7aa5c2e965e3a6c7ad851a
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-04-27 15:09:19 -0300

    agrego external a vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/storage/css/calendario.css',
+        '/storage/css/cardevento.css',
+        '/storage/css/comollegar.css',
+        '/storage/css/detalleevento.css',
+        '/storage/css/footer.css',
+        '/storage/css/header.css',
+        '/storage/css/historia.css',
+        '/storage/css/index.css',
+        '/storage/css/navbar.css',
+        '/storage/css/preguntasfrecuentes.css',
+        '/storage/css/sliderdestacado.css',
+        '/storage/css/tablaprecios.css'
+      ]
+    }
+  }
 })
