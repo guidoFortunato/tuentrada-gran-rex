83cc53c5a33a962dbbfbecfac41485c753a5b356
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-05-15 12:43:22 -0300

    mejoro getEvents, no vuelvo a hacer solicitud Post pidiedo token

--- src/helpers/getInfoGeneral.js
+++ src/helpers/getInfoGeneral.js
@@ -3,95 +3,96 @@ import { getEnvVariables } from "./getEnvVariables";
 
 const { VITE_API_INFO_GENERAL, VITE_EMAIL, VITE_PASS } = getEnvVariables();
 
-
 export const getInfoGeneral = async (venue = "Ituzaingo") => {
+  const URL = VITE_API_INFO_GENERAL + venue;
+  const email = VITE_EMAIL;
+  const password = VITE_PASS;
+  const timeNow = Date.now();
+  const tokenStorage = localStorage.getItem("tokenStorage");
+  const expiresTokenStorage = localStorage.getItem("expiresTokenStorage");
+	console.log('--------------------GetInfoGeneral--------------------------------')
+  console.log("timeNow: " + timeNow);
+  console.log("expiresTokenStorage: " + expiresTokenStorage);
 
-	const URL = VITE_API_INFO_GENERAL + venue;
-	const email = VITE_EMAIL;
-	const password = VITE_PASS;
-	const timeNow = Date.now();
-	const tokenStorage = localStorage.getItem('tokenStorage');
-	const expiresTokenStorage = localStorage.getItem('expiresTokenStorage');
-	console.log('timeNow: ' + timeNow);
-	console.log('expiresTokenStorage: ' + expiresTokenStorage);
+  if (!tokenStorage) {
+    const { token, tokenExpires } = await getToken(email, password);
+    console.log("tokenStorage no existe");
+    console.log("pido un nuevo token: " + token);
+    try {
+      const response = await fetch(URL, {
+        credentials: "include",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          // 'Access-Control-Allow-Origin': '*'
+        },
+      });
+      // console.log({response});
 
-	if (!tokenStorage) {
-		const { token, tokenExpires } = await getToken(email, password);
-		console.log('tokenStorage no existe');
-		console.log('pido un nuevo token: ' + token);
-		try {
-			const response = await fetch(URL, {
-				credentials: 'include',
-				method: 'POST',
-				headers: {
-					Authorization: `Bearer ${token}`,
-					// 'Access-Control-Allow-Origin': '*'
-				},
-			});
-			// console.log({response});
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
 
-			if (!response.ok) {
-				throw new Error(`${response.status} - ${response.statusText}`);
-			}
+      localStorage.setItem("tokenStorage", token);
+      localStorage.setItem("expiresTokenStorage", tokenExpires);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    if (tokenStorage && expiresTokenStorage) {
+      if (timeNow > expiresTokenStorage) {
+        console.log("tokenStorage existe y expiró: ");
+        console.log("pido nuevo token");
+        const { token, tokenExpires } = await getToken(email, password);
+        try {
+          const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          // console.log({response});
 
-			localStorage.setItem('tokenStorage', token);
-			localStorage.setItem('expiresTokenStorage', tokenExpires);
-			const data = await response.json();
-			return data;
-		} catch (error) {
-			throw new Error(error);
-		}
-	} else {
-		if (tokenStorage && expiresTokenStorage) {
-			if (timeNow > expiresTokenStorage) {
-				console.log('tokenStorage existe y expiró: ');
-				console.log('pido nuevo token');
-				const { token, tokenExpires } = await getToken(email, password);
-				try {
-					const response = await fetch(URL, {
-						method: 'POST',
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					});
-					// console.log({response});
+          if (!response.ok) {
+            throw new Error(
+              `Error: ${response.status}. ${response.statusText}`
+            );
+          }
+          localStorage.setItem("tokenStorage", token);
+          localStorage.setItem("expiresTokenStorage", tokenExpires);
+          const data = await response.json();
+          return data;
+        } catch (error) {
+          throw new Error(error);
+        }
+      } else {
+        console.log("tokenStorage existe y NO expiró");
+        console.log(
+          "realizo la solicitud con el tokenStorage: " + tokenStorage
+        );
+        try {
+          const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${tokenStorage}`,
+            },
+          });
+          // console.log({response});
 
-					if (!response.ok) {
-						throw new Error(
-							`Error: ${response.status}. ${response.statusText}`
-						);
-					}
-					localStorage.setItem('tokenStorage', token);
-					localStorage.setItem('expiresTokenStorage', tokenExpires);
-					const data = await response.json();
-					return data;
-				} catch (error) {
-					throw new Error(error);
-				}
-			} else {
-                console.log('tokenStorage existe y NO expiró');
-                console.log('realizo la solicitud con el tokenStorage: ' + tokenStorage);
-				try {
-					const response = await fetch(URL, {
-						method: 'POST',
-						headers: {
-							Authorization: `Bearer ${tokenStorage}`,
-						},
-					});
-					// console.log({response});
+          if (!response.ok) {
+            throw new Error(
+              `Error: ${response.status}. ${response.statusText}`
+            );
+          }
 
-					if (!response.ok) {
-						throw new Error(
-							`Error: ${response.status}. ${response.statusText}`
-						);
-					}
-
-					const data = await response.json();
-					return data;
-				} catch (error) {
-					throw new Error(error);
-				}
-			}
-		}
-	}
+          const data = await response.json();
+          return data;
+        } catch (error) {
+          throw new Error(error);
+        }
+      }
+    }
+  }
 };
