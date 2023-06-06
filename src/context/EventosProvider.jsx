1baf10de4e43211a85ca9863c4c26a6a8c420982
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-06-06 15:40:32 -0300

    version - pre api

--- src/context/EventosProvider.jsx
+++ src/context/EventosProvider.jsx
@@ -4,12 +4,12 @@ import { getEnvVariables, useFetch } from "../helpers";
 export const EventosContext = createContext();
 
 const eventosBusqueda = [];
-const urlEventos = "/storage/json/eventos.json";
-const urlNavbar = "/storage/json/navbar.json";
-const urlFooter = "/storage/json/footer.json";
-// const urlTestEventos = "/src/json/eventosTest.json";
-// const urlTestNavbar = "/src/json/navbarTest.json";
-// const urlTestFooter = "/src/json/footerTest.json";
+// const urlEventos = "/storage/json/eventos.json";
+// const urlNavbar = "/storage/json/navbar.json";
+// const urlFooter = "/storage/json/footer.json";
+const urlTestEventos = "/src/json/eventosTest.json";
+const urlTestNavbar = "/src/json/navbarTest.json";
+const urlTestFooter = "/src/json/footerTest.json";
 // const { VITE_JSON_EVENTOS } = getEnvVariables();
 // const { VITE_JSON_FOOTER } = getEnvVariables();
 // const { VITE_JSON_NAVBAR } = getEnvVariables();
@@ -17,7 +17,7 @@ const urlFooter = "/storage/json/footer.json";
 const EventosProvider = (props) => {
   const [evento, setEvento] = useState("");
   const [listaEventosBusqueda, setListaEventosBusqueda] =  useState(eventosBusqueda);
-  const { data: dataEventos, isLoading, hasError } = useFetch(urlEventos);
+  const { data: dataEventos, isLoading, hasError } = useFetch(urlTestEventos);
   const { data: dataNavbar, isLoading: isLoadingNavbar } =  useFetch( urlNavbar );
   const { data: dataFooter, isLoading: isLoadingFooter } =  useFetch( urlFooter );
 
