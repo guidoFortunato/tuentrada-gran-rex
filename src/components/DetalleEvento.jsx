0882510c7fb371791ba3b05f862fe2be234fb504
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-06-08 11:51:19 -0300

    accordion flowbite

--- src/components/DetalleEvento.jsx
+++ src/components/DetalleEvento.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Accordion } from "flowbite-react";
 // import Modal from "react-modal";
 
 import { EventosContext } from "../context/EventosProvider";
@@ -64,7 +65,7 @@ export const DetalleEvento = () => {
 
   return (
     <>
-      <div className="container mx-auto my-5 px-3 lg:px-0">
+      <div className="container mx-auto my-5 px-3 ">
         <section
           className={`bg-[url("assets/images/responsive.jpg")]  lg:bg-[url("assets/images/desktop.jpg")]  bg-no-repeat bg-cover bg-center mx-auto`}
         >
@@ -86,6 +87,104 @@ export const DetalleEvento = () => {
             }}
           ></p>
         </div>
+
+        <Accordion data-accordion="open">
+          <Accordion.Panel >
+            <Accordion.Title>Fechas</Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                
+                  Flowbite is an open-source library of interactive components
+                  built on top of Tailwind CSS including buttons, dropdowns,
+                  modals, navbars, and more.
+                
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                Check out this guide to learn how to
+                <a
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  href="https://flowbite.com/docs/getting-started/introduction/"
+                >
+                  get started
+                </a>
+               
+                  and start developing websites even faster with components on
+                  top of Tailwind CSS.
+               
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                
+                  Flowbite is first conceptualized and designed using the Figma
+                  software so everything you see in the library has a design
+                  equivalent in our Figma file.
+                
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                Check out the
+                <a
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                  href="https://flowbite.com/figma/"
+                >
+                  Figma design system
+                </a>
+                
+                  based on the utility classes from Tailwind CSS and components
+                  from Flowbite.
+              
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              What are the differences between Flowbite and Tailwind UI?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                
+                  The main difference is that the core components from Flowbite
+                  are open source under the MIT license, whereas Tailwind UI is
+                  a paid product. Another difference is that Flowbite relies on
+                  smaller and standalone components, whereas Tailwind UI offers
+                  sections of pages.
+                
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                
+                  However, we actually recommend using both Flowbite, Flowbite
+                  Pro, and even Tailwind UI as there is no technical reason
+                  stopping you from using the best of two worlds.
+               
+              </p>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Learn more about these technologies:
+              </p>
+              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+                <li>
+                  <a
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                    href="https://flowbite.com/pro/"
+                  >
+                    <p>Flowbite Pro</p>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-cyan-600 hover:underline dark:text-cyan-500"
+                    href="https://tailwindui.com/"
+                    rel="nofollow"
+                  >
+                    <p>Tailwind UI</p>
+                  </a>
+                </li>
+              </ul>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
       </div>
       {/* <div
         className="container my-5 px-5 animate_animated animatefadeIn animate_fast"
