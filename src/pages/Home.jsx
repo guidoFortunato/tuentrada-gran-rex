bd466cfef19898faf8de84fb831b68927c266e62
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-06-08 16:14:13 -0300

    cambio en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,69 +44,67 @@ export const Home = () => {
       <>
         <header
           style={{ backgroundImage: `url(${dataInfoGeneral.backgroundImage})` }}
-          className={`bg-no-repeat bg-cover bg-center container mx-auto `}
+          className={`bg-no-repeat bg-cover bg-center container mx-auto`}
         >
-         <div
-          className={`min-h-[50vh] flex justify-center items-start flex-col ps-4 `}
-        >
-          <h1
-            style={{ color: dataInfoGeneral.colorH1, paddingBottom: "20px" }}
-            className={`m-0 text-5xl titulo-principal`}
+          <div
+            className={`min-h-[50vh] flex justify-center items-start flex-col ps-4`}
           >
-            <strong>{dataInfoGeneral.pages[0].title}</strong>
-          </h1>
-          <div className="flex flex-col md:flex-row">
-            <p
-              style={{ color: dataInfoGeneral.colorButton }}
-              className="md:w-4/5 md:mr-6 mt-6 md:mt-0 text-[dataInfoGeneral.colorButton]"
+            <h1
+              style={{ color: dataInfoGeneral.colorH1, paddingBottom: "20px" }}
+              className={`m-0 text-5xl titulo-principal`}
             >
-              ¡No te pierdas ni un solo evento! Encontrá toda la programación
-              completa en el siguiente enlace y preparate para vivir
-              experiencias únicas. ¡Descubrí tus próximos momentos inolvidables
-              aquí!
-            </p>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <button
-                style={{
-                  color: dataInfoGeneral.colorButton,
-                  background: dataInfoGeneral.backgroundButton,
-                  width: "auto",
-                }}
-                type="button"
-                className="w-full md:w-auto mt-6 md:mt-0 bg-[dataInfoGeneral.backgroundButton] hover:bg-[#5c452c] hover:border-[dataInfoGeneral.colorHoverButton] text-[dataInfoGeneral.colorButton] font-medium rounded-3xl text-sm px-4 py-2"
+              <strong>{dataInfoGeneral.pages[0].title}</strong>
+            </h1>
+            <div className="flex flex-col md:flex-row">
+              <p
+                style={{ color: dataInfoGeneral.colorButton }}
+                className="md:w-4/5 md:mr-6 mt-6 md:mt-0 text-[dataInfoGeneral.colorButton]"
               >
-                Calendario
-              </button>
-            </div>
-          </div>
-        </div>
-        </header>
-        <main className="container mx-auto my-14 px-3 lg:px-0">
-          
-            <div
-              className="w-1/2 flex justify-center mx-auto p-4 mb-4 text-md text-blue-900 rounded-lg bg-blue-300 dark:bg-gray-800 dark:text-blue-400"
-              role="alert"
-            >
-              <svg
-                aria-hidden="true"
-                className="flex-shrink-0 inline w-5 h-5 mr-3"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Info</span>
-              <div>
-                <span className="font-medium"></span>
-                No hay eventos disponibles
+                ¡No te pierdas ni un solo evento! Encontrá toda la programación
+                completa en el siguiente enlace y preparate para vivir
+                experiencias únicas. ¡Descubrí tus próximos momentos
+                inolvidables aquí!
+              </p>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <button
+                  style={{
+                    color: dataInfoGeneral.colorButton,
+                    background: dataInfoGeneral.backgroundButton,
+                    width: "auto",
+                  }}
+                  type="button"
+                  className="w-full md:w-auto mt-6 md:mt-0 bg-[dataInfoGeneral.backgroundButton] hover:bg-[#5c452c] hover:border-[dataInfoGeneral.colorHoverButton] text-[dataInfoGeneral.colorButton] font-medium rounded-3xl text-sm px-4 py-2"
+                >
+                  Calendario
+                </button>
               </div>
             </div>
-         
+          </div>
+        </header>
+        <main className="container mx-auto my-14 px-3 lg:px-0">
+          <div
+            className="w-1/2 flex justify-center mx-auto p-4 mb-4 text-md text-blue-900 rounded-lg bg-blue-300 dark:bg-gray-800 dark:text-blue-400"
+            role="alert"
+          >
+            <svg
+              aria-hidden="true"
+              className="flex-shrink-0 inline w-5 h-5 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium"></span>
+              No hay eventos disponibles
+            </div>
+          </div>
         </main>
       </>
     );
@@ -118,44 +116,56 @@ export const Home = () => {
         style={{ backgroundImage: `url(${dataInfoGeneral.backgroundImage})` }}
         className={`bg-no-repeat bg-cover bg-center container mx-auto `}
       >
-        
-       <div className={`min-h-[50vh] flex justify-center items-start flex-col `}>
-        <div style={{display:"flex", justifyContent:"center", width:"100%"}}>
-  <h1
-    style={{ color: dataInfoGeneral.colorH1, fontSize:"2.5em", width:"92%", }}
-    className={`m-0 text-5xl titulo-principal pt-10 pb-5  text-center md:text-left`}
-  >
-    <strong>{dataInfoGeneral.pages[0].title}</strong>
-  </h1>  
-  </div>
-  <div className="flex flex-col md:flex-row" style={{justifyContent:"center", alignItems:"center"}}>
-    <p
-      style={{ color: dataInfoGeneral.colorButton, }}
-      className={`md:w-3/5 md:mr-6 pl-10 pr-10 md:pl-0 md:pr-0 text-base lg:text-lg`}
-    >
-      ¡No te pierdas ni un solo evento! Encontrá toda la programación
-      completa en el siguiente enlace y preparate para vivir
-      experiencias únicas. ¡Descubrí tus próximos momentos inolvidables
-      aquí!
-    </p>
-    <div className="flex md:flex-col md:items-center md:justify-center mb-10 " style={{ width: "30%", justifyContent:"center",}}>
-      <button
-        style={{
-          color: dataInfoGeneral.colorButton,
-          background: dataInfoGeneral.backgroundButton,
-          width: "auto",
-          padding: "10px 45px",
-      
-        }}
-        type="button"
-        className="w-full mt-6 md:mt-0 bg-[dataInfoGeneral.backgroundButton] hover:bg-[#5c452c] hover:border-[dataInfoGeneral.colorHoverButton] text-[dataInfoGeneral.colorButton] font-medium rounded-3xl text-sm px-4 py-2 text-base lg:text-lg"
-      >
-        Calendario
-      </button>
-    </div>
-  </div>
-</div>
-
+        <div
+          className={`min-h-[50vh] flex justify-center items-start flex-col `}
+          style={{background: 'rgba(0, 0, 0, 0.4)'}}
+        >
+          <div
+            className="flex justify-center w-full"
+          >
+            <h1
+              style={{
+                color: dataInfoGeneral.colorH1,
+                fontSize: "2.5em",
+                width: "92%",
+              }}
+              className={`m-0 text-5xl titulo-principal pt-10 pb-5  text-center md:text-left`}
+            >
+              <strong>{dataInfoGeneral.pages[0].title}</strong>
+            </h1>
+          </div>
+          <div
+            className="flex flex-col md:flex-row"
+            style={{ justifyContent: "center", alignItems: "center" }}
+          >
+            <p
+              style={{ color: dataInfoGeneral.colorButton }}
+              className={`md:w-3/5 md:mr-6 pl-10 pr-10 md:pl-0 md:pr-0 text-base lg:text-lg`}
+            >
+              ¡No te pierdas ni un solo evento! Encontrá toda la programación
+              completa en el siguiente enlace y preparate para vivir
+              experiencias únicas. ¡Descubrí tus próximos momentos inolvidables
+              aquí!
+            </p>
+            <div
+              className="flex md:flex-col md:items-center md:justify-center mb-10 "
+              style={{ width: "30%", justifyContent: "center" }}
+            >
+              <button
+                style={{
+                  color: dataInfoGeneral.colorButton,
+                  background: dataInfoGeneral.backgroundButton,
+                  width: "auto",
+                  padding: "10px 45px",
+                }}
+                type="button"
+                className="w-full mt-6 md:mt-0 bg-[dataInfoGeneral.backgroundButton] hover:bg-[#5c452c] hover:border-[dataInfoGeneral.colorHoverButton] text-[dataInfoGeneral.colorButton] font-medium rounded-3xl text-sm px-4 py-2 text-base lg:text-lg"
+              >
+                Calendario
+              </button>
+            </div>
+          </div>
+        </div>
       </header>
       <main className="container mx-auto my-14 px-3 lg:px-0 ">
         <InfiniteScroll
@@ -165,7 +175,6 @@ export const Home = () => {
           loader={<Spinner />}
         >
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10">
-                    
             {eventos.map((item) => (
               <CardEvento
                 linkEvento={item.slug + "/" + item.id}
