6d6e1c3ffc7fa333c2ddd7c890b0d9b548d908ad
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-06-19 17:09:06 -0300

    plano en modal

--- src/components/ModalPrecios.jsx
+++ src/components/ModalPrecios.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Modal } from "flowbite-react";
 import { TablaPrecios } from "./";
+import { EventosContext } from "../context/EventosProvider";
 
 export const ModalPrecios = ({ performances, openModal, setOpenModal }) => {
+  const { dataInfoGeneral } = useContext(EventosContext);
+  console.log({ dataInfoGeneral });
+  console.log({ performances });
   return (
     <>
-      <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+      <Modal dismissible show={openModal} size={"4xl"} onClose={() => setOpenModal(false)}>
         <Modal.Header>Precios y Ubicaciones</Modal.Header>
         <Modal.Body>
-          <div className="space-y-6">
-            <div className="relative overflow-x-auto">
-              <TablaPrecios performances={performances} />
+          <div className="space-y-6 w-full">
+            <div className="relative overflow-x-auto grid grid-cols-1 lg:grid-cols-2 gap-6">
+              <div>
+                <TablaPrecios performances={performances} />
+              </div>
+              <div>
+                <img
+                  src={performances.venueImageMedium}
+                  alt={`plano ${dataInfoGeneral.pages[0].title.toLowerCase()}`}
+                  className="w-full object-cover"
+                />
+              </div>
             </div>
           </div>
         </Modal.Body>
