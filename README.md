2abb3bbd34ae596ebc656a5ee4a0974b60f3570c
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-07-19 18:30:41 -0300

    actualizo el readme y si el internalState de las fechas es "sin fechas" no se muestran dichas fechas dentro del product

--- README.md
+++ README.md
@@ -35,3 +35,5 @@
 
 17. cuando buscas un evento muy largo se pone por debajo del boton buscar
 
+18. no aparecen los segundos en editar fecha en backend
+
--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -59,7 +59,7 @@ export const Accordion = ({ itemsAccordion, dataEvento }) => {
           >
             <div className="flex flex-col max-h-90 overflow-y-auto border-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900 text-sm">
               {newPerformances.map((item) => {
-                if (item.internalState !== "soon") {
+                if (item.internalState !== "soon" && item.internalState !== "sin fechas") {
                   return (
                     <FechaEvento
                       dataFechas={item}
