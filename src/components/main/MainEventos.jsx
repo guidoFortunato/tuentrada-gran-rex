7551df0710d738eef68befde703806b8c45376c4
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-08-08 16:04:48 -0300

    cambios

--- src/components/main/MainEventos.jsx
+++ src/components/main/MainEventos.jsx
@@ -13,7 +13,6 @@ export const MainEventos = () => {
         dataLength={eventosGenerales.length}
         next={() => setPage((prevPage) => prevPage + 1)}
         hasMore={data.links.next !== null}
-        // hasMore={data.products}
         loader={<Spinner />}
       >
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10 px-4 lg:px-0">
