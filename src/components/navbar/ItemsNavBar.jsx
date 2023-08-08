df3dbab85e285a386114ca6d58596aa36ad0995d
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-08-08 14:56:54 -0300

    cambio estilo hover

--- src/components/navbar/ItemsNavBar.jsx
+++ src/components/navbar/ItemsNavBar.jsx
@@ -9,30 +9,22 @@ export const ItemsNavBar = ({ item, handleButtonsCollapse }) => {
   return (
     <>
       <li key={item.id} className={item.type.name !== "Home" ? "lg:border-l-2 lg:border-black lg:pl-8" : ""}>
-     <NavLink
+      <NavLink
   to={item.path}
-  exact // Añadir la prop exact para que se aplique solo en la ruta exacta
   onClick={() => handleButtonsCollapse()}
   style={{
-    color: location.pathname === item.path ? dataInfoGeneral.colorHoverLi : dataInfoGeneral.colorLi,
+    color: dataInfoGeneral.colorLi, // Color original
     backgroundColor: 'transparent', // Puedes ajustar esto según tu diseño
     transition: 'color 0.3s', // Transición suave para el color
-  }}
-  activeClassName="active" // Clase CSS para el estado activo
-  isActive={(match, location) => {
-    // Aquí defines la lógica para determinar si el enlace está activo
-    // Por ejemplo, puedes comparar location.pathname con item.path
-    return match || location.pathname === item.path;
   }}
   onMouseOver={(e) => {
     e.target.style.color = dataInfoGeneral.colorHoverLi; // Cambiar al color de hover
   }}
   onMouseOut={(e) => {
-    if (location.pathname !== item.path) {
-      e.target.style.color = dataInfoGeneral.colorLi; // Restaurar el color original si no está activo
-    }
+    e.target.style.color = dataInfoGeneral.colorLi; // Restaurar el color original
   }}
-  className="p-0 font-semibold transition-all duration-300 ease-in-out"
+
+  className="p-0 font-semibold lg:hover:text-[#8b663c] transition-all duration-300 ease-in-out"
 >
   {item.type.name}
 </NavLink>
