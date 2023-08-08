b984157e6b96154cb60c8dfdf5b2be54e0bae473
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-08-08 14:34:20 -0300

    info api nueva

--- src/components/main/MainEventos.jsx
+++ src/components/main/MainEventos.jsx
@@ -12,8 +12,8 @@ export const MainEventos = () => {
       <InfiniteScroll
         dataLength={eventosGenerales.length}
         next={() => setPage((prevPage) => prevPage + 1)}
-        // hasMore={data.links.next !== null}
-        hasMore={data.products}
+        hasMore={data.links.next !== null}
+        // hasMore={data.products}
         loader={<Spinner />}
       >
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-10 px-4 lg:px-0">
