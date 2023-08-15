3a351aca3b64588fbace90686945e04a34332ce7
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-08-15 17:59:55 -0300

    redes en la descripcion

--- src/components/detalleEvento/DetalleEvento.jsx
+++ src/components/detalleEvento/DetalleEvento.jsx
@@ -103,22 +103,23 @@ export const DetalleEvento = () => {
                   __html: DOMPurify.sanitize(data.product.description),
                 }}
               ></p>
-            </div>
-          </div>
-        </div>
-
-        <Accordion itemsAccordion={itemsAccordion} dataEvento={data} />
-
-        {data.product.socialNetworks.length > 0 && (
-          <div className="mt-5 flex flex-col items-center">
-            <h3 className="text-[#6b7280] font-semibold text-lg lg:text-xl w-full text-center">
+               {data.product.socialNetworks.length > 0 && (
+          <div className="mt-5 flex flex-col items-center lg:items-start">
+            <h3 className="text-[#6b7280] font-semibold text-lg lg:text-xl w-full text-center lg:text-start">
               Seguilo en sus redes
             </h3>
             <ul className="flex space-x-3 my-2">
               <RedesSociales dataRedes={data.product.socialNetworks} />
             </ul>
           </div>
-        )}
+        )}  
+            </div>
+          </div>
+        </div>
+       
+        <Accordion itemsAccordion={itemsAccordion} dataEvento={data} />
+
+       
       </div>
     </div>
   );
