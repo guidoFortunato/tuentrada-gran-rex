f7e7a535c1b2577b0105bc7e7464fab876aff42b
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-08-18 13:32:13 -0300

    tamaño letra

--- src/components/disponibility/EventAvailable.jsx
+++ src/components/disponibility/EventAvailable.jsx
@@ -34,7 +34,7 @@ export const EventAvailable = ({
               : disponibility === "LIMITED"
               ? "bg-pink-800"
               : "bg-red-600"
-          } z-10 absolute -right-0 -top-4 text-white rounded text-xs xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
+          } z-10 absolute -right-0 -top-4 text-white rounded text-xs 2xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
         >
           {disponibility === "GOOD"
             ? "Disponible"
@@ -44,7 +44,7 @@ export const EventAvailable = ({
         </button>
         <Link to={linkEvento}>
           <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
-            <p className="flex items-end justify-center text-xs xl:text-base uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
+            <p className="flex items-end justify-center text-xs 2xl:text-sm uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
               {title}
             </p>
           </figcaption>
--- src/components/disponibility/EventCancel.jsx
+++ src/components/disponibility/EventCancel.jsx
@@ -22,18 +22,13 @@ export const EventCancel = ({ linkEvento, imgApi, title, imgSTXVeryLarge }) => {
         </Link>
         <button
           type="button"
-          className={`bg-black z-10 absolute -right-0 -top-4 text-white rounded text-xs xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
+          className={`bg-black z-10 absolute -right-0 -top-4 text-white rounded text-xs 2xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
         >
           Cancelado
         </button>
         <Link to={linkEvento}>
-<<<<<<< HEAD
-          <figcaption className=" opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgba(15,15,15,0.82)] hover:to-[rgba(173,170,167,0.15)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
-            <p className="flex items-end text-xs xl:text-base text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
-=======
         <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
-            <p className="flex items-end justify-center text-xs uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
->>>>>>> 457657b1fdefb0c355d5c547d3f8d5fc0988c3c4
+            <p className="flex items-end justify-center text-xs 2xl:text-sm uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
               {title}
             </p>
           </figcaption>
