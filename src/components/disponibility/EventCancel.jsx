534ec38c1b7c1089de919316815259131de82578
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-09-05 16:45:00 -0300

    Se editó el nombre en las cards

--- src/components/disponibility/EventCancel.jsx
+++ src/components/disponibility/EventCancel.jsx
@@ -3,37 +3,72 @@ import { Link } from "react-router-dom";
 export const EventCancel = ({ linkEvento, imgApi, title, imgSTXVeryLarge }) => {
   // console.log({linkEvento})
   return (
+    // <>
+    //   <figure className="relative max-w-sm cursor-pointer border rounded-lg border-gray-300 ">
+    //     <Link to={linkEvento}>
+    //       {imgApi ? (
+    //         <img
+    //           className="rounded-lg md:rounded-none"
+    //           src={imgApi}
+    //           alt={title}
+    //         />
+    //       ) : (
+    //         <img
+    //           className="rounded-lg md:rounded-none"
+    //           src={imgSTXVeryLarge}
+    //           alt={title}
+    //         />
+    //       )}
+    //     </Link>
+    //     <button
+    //       type="button"
+    //       className={`bg-black z-10 absolute -right-0 -top-4 text-white rounded text-xs 2xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
+    //     >
+    //       Cancelado
+    //     </button>
+    //     <Link to={linkEvento}>
+    //     <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
+    //         <p className="flex items-end justify-center text-xs 2xl:text-sm uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
+    //           {title}
+    //         </p>
+    //       </figcaption>
+    //     </Link>
+    //   </figure>
+    // </>
     <>
-      <figure className="relative max-w-sm cursor-pointer border rounded-lg border-gray-300 ">
-        <Link to={linkEvento}>
-          {imgApi ? (
-            <img
-              className="rounded-lg md:rounded-none"
-              src={imgApi}
-              alt={title}
-            />
-          ) : (
-            <img
-              className="rounded-lg md:rounded-none"
-              src={imgSTXVeryLarge}
-              alt={title}
-            />
-          )}
-        </Link>
-        <button
-          type="button"
-          className={`bg-black z-10 absolute -right-0 -top-4 text-white rounded text-xs 2xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
-        >
-          Cancelado
-        </button>
-        <Link to={linkEvento}>
-        <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
-            <p className="flex items-end justify-center text-xs 2xl:text-sm uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
-              {title}
-            </p>
-          </figcaption>
-        </Link>
-      </figure>
-    </>
+     <figure className="relative max-w-sm cursor-pointer   ">
+      <div className="overflow-hidden rounded-lg border  border-gray-300">
+    <div className="  rounded-lg   hover:scale-105 transition-all ease-in-out duration-500" >
+      <Link to={linkEvento}>
+        {imgApi ? (
+          <img
+            className="rounded-lg"
+            src={imgApi}
+            alt={title}
+          />
+        ) : (
+          <img
+            className="rounded-lg"
+            src={imgSTXVeryLarge}
+            alt={title}
+          />
+        )}
+     
+      </Link>
+      </div>
+      </div>
+      <button
+        type="button"
+        className={`bg-black z-10 absolute -right-0 -top-4 text-white rounded text-xs 2xl:text-sm px-2 py-1 lg:py-1.5 text-center mr-2 mb-2`}
+      >
+        Cancelado
+      </button>
+      <Link to={linkEvento}>
+      <p className="text-center text-xs 2xl:text-sm uppercase  text-black font-bold p-1">
+            {title}
+          </p>
+      </Link>
+    </figure>
+  </>
   );
 };
--- src/components/disponibility/EventNotAvailable.jsx
+++ src/components/disponibility/EventNotAvailable.jsx
@@ -4,40 +4,81 @@ export const EventNotAvailable = ({
   imgSTXVeryLarge,
 }) => {
   return (
-    <>
-      <figure className="relative max-w-sm cursor-default border rounded-lg border-gray-300 ">
-        <div>
-          {imgApi ? (
-            <img
-              className="rounded-lg md:rounded-none opacity-40"
-              src={imgApi}
-              alt={title}
-            />
-          ) : (
-            <img
-              className="rounded-lg md:rounded-none opacity-40"
-              src={imgSTXVeryLarge}
-              alt={title}
-            />
-          )}
-          {/* <img
-            className="rounded-lg md:rounded-none opacity-40"
+    // <>
+    //   <figure className="relative max-w-sm cursor-default border rounded-lg border-gray-300 ">
+    //     <div>
+    //       {imgApi ? (
+    //         <img
+    //           className="rounded-lg md:rounded-none opacity-40"
+    //           src={imgApi}
+    //           alt={title}
+    //         />
+    //       ) : (
+    //         <img
+    //           className="rounded-lg md:rounded-none opacity-40"
+    //           src={imgSTXVeryLarge}
+    //           alt={title}
+    //         />
+    //       )}
+    //       {/* <img
+    //         className="rounded-lg md:rounded-none opacity-40"
+    //         src={imgApi}
+    //         alt={title}
+    //       /> */}
+    //     </div>
+    //     <button
+    //       type="button"
+    //       className="z-10 absolute -right-0 -top-4 text-white bg-gray-600 cursor-default rounded text-xs px-2 py-1 lg:py-1.5 text-center mr-2 mb-2"
+    //     >
+    //       No disponible
+    //     </button>
+    //     <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
+    //         <p className="flex items-end justify-center text-xs uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
+    //           {title}
+    //         </p>
+    //       </figcaption>
+    //   </figure>
+    // </>
+       <>
+       <figure className="relative max-w-sm cursor-default ">
+       <div className="overflow-hidden rounded-lg border  border-gray-300">
+    <div className="  rounded-lg   hover:scale-105 transition-all ease-in-out duration-500" >
+      <Link to={linkEvento}>
+        {imgApi ? (
+          <img
+            className="rounded-lg opacity-40"
             src={imgApi}
             alt={title}
-          /> */}
-        </div>
-        <button
-          type="button"
-          className="z-10 absolute -right-0 -top-4 text-white bg-gray-600 cursor-default rounded text-xs px-2 py-1 lg:py-1.5 text-center mr-2 mb-2"
-        >
-          No disponible
-        </button>
-        <figcaption className="rounded-lg opacity-0 hover:opacity-100 transition duration-500 ease-in-out h-full bg-gradient-to-t hover:from-[rgb(0,0,0)] hover:to-[rgba(30,16,3,0.13)] hover:bg-gradient-to-t absolute bottom-0 right-0 left-0 top-0">
-            <p className="flex items-end justify-center text-xs uppercase  text-white h-full p-5 font-bold hover:transition-colors duration-500 ease-in-out hover:text-white">
-              {title}
-            </p>
-          </figcaption>
-      </figure>
-    </>
+          />
+        ) : (
+          <img
+            className="rounded-lg opacity-40"
+            src={imgSTXVeryLarge}
+            alt={title}
+         
+            />
+           
+        )}
+     
+      </Link>
+       {/* <img
+    //         className="rounded-lg md:rounded-none opacity-40"
+    //         src={imgApi}
+    //         alt={title}
+    //       /> */}
+      </div>
+      </div> 
+        
+         <button
+           type="button"
+           className="z-10 absolute -right-0 -top-4 text-white bg-gray-600 cursor-default rounded text-xs px-2 py-1 lg:py-1.5 text-center mr-2 mb-2"
+         >
+           No disponible
+         </button>
+         <p className="text-center text-xs 2xl:text-sm uppercase  text-black font-bold p-1">
+            {title}
+          </p>
+       </figure>
+     </>
   );
 };
